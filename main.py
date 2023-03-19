7f7bde4e51f48ec2881686dab155499a047a1148
anitasulca <125455508+anitasulca@users.noreply.github.com>
2023-03-19 15:57:01 +0000

    Tests

--- main.py
+++ main.py
@@ -1,30 +1,26 @@
 # python3
 
 def parallel_processing(n, m, data):
-    # create a list of tuples to store the finishing time and thread index for each job
-    finish_time = [(0, i) for i in range(n)]
+    time = [(0, i) for i in range(n)]
     output = []
 
     for i in range(m):
-        # find the thread with the earliest finishing time
-        next_thread = min(finish_time)
-        # update the finish time for the chosen thread
-        finish_time[next_thread[1]] = (next_thread[0] + data[i], next_thread[1])
-        # add the output pair to the list
-        output.append((next_thread[1], next_thread[0]))
+        thread = min(time)
+        time[thread[1]] = (thread[0] + data[i], thread[1])
+        output.append((thread[1], thread[0]))
 
     return output
 
 
 def main():
-    # read input from keyboard
+
     n, m = map(int, input().split())
     data = list(map(int, input().split()))
 
-    # call the parallel_processing function
+  
     result = parallel_processing(n, m, data)
 
-    # print the output
+
     for i in range(m):
         print(result[i][0], result[i][1])
 
